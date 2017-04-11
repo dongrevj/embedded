fd996d046b5db8824053339c217d5421615727fe
dongrevj <dongrevj@yahoo.co.in>
2017-04-11 09:23:39 +0530

    Add files via upload

---
+++ boolean.ino
@@ -0,0 +1,22 @@
+int LEDpin = 13;       // LED on pin 5
+int switchPin = 6;   // momentary switch on 13, other side connected to ground
+
+boolean running = false;
+
+void setup()
+{
+  pinMode(LEDpin, OUTPUT);
+  pinMode(switchPin, INPUT);
+  digitalWrite(switchPin, HIGH);      // turn on pullup resistor
+}
+
+void loop()
+{
+  if (digitalRead(switchPin) == LOW)
+  {  // switch is pressed - pullup keeps pin high normally
+    delay(100);                        // delay to debounce switch
+    running = !running;                // toggle running variable
+    digitalWrite(LEDpin, running);      // indicate via LED
+  }
+}
+
---
+++ sensor.ino
@@ -0,0 +1,16 @@
+
+const int SensePin = 12; 
+int SenseState =0; 
+void setup() {
+  
+pinMode(SensePin, INPUT);
+pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop() {
+  SenseState = digitalRead(SensePin);
+if (SenseState ==1)
+      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
+else
+      digitalWrite(LED_BUILTIN, LOW);   // turn the LED off 
+ 
+}
